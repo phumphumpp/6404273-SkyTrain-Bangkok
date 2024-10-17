ebce76234e166f9a5fb17d61b870f3828a8e1f8f
phumphumpp <phumphumppp@gmail.com>
2024-10-18 01:40:07 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Main from "./components/Main"; // Import the Main component
+
+export default function App() {
+  return (
+    <>
+      <Navbar />  {/* This renders the Navbar */}
+      <Main />    {/* This renders the Main content */}
+    </>
+  );
+}
